47757dc4110b3f2823c9fa35b59403a0e2aa37fa
Lee, Seulah <sarhee1004@naver.com>
2024-10-16 04:45:07 +0900

    quick_sort 구현 중
    partition 메소드를 구현해야함

--- src/git_test3/Sort.java
+++ src/git_test3/Sort.java
@@ -2,16 +2,22 @@ package git_test3;
 
 public class Sort {
 	public static void sort(int[] a) {
-		for (int i=1; i<a.length; ++i) {
-			int value = a[i];
-			int j;
-			for (j=1-1; j>=0; --j)
-				if (a[j] > value)
-					a[j+1] = a[j];
-				else
-					break;
-			a[j+1] = value;
-		}
+		quickSort(a, 0, a.length-1);
 	}
+	static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+	static int partition(int[] a, int start, int end) {
+		// TODO: 알고리즘 구현
+	}
+	static void quickSort(int[] a, int start, int end) {
+		if (start >= end) return;
+		int middle = partition(a, start, end);
+		quickSort(a, start, middle-1);
+		quickSort(a, middle+1, end);
+	} 
 
 }
